20e79c0f5b0cea06e2a31ae1273a63a19f48e576
Barhuda <64771963+Barhuda@users.noreply.github.com>
2021-05-03 10:24:38 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const request = require("request");
+const client = require("@mailchimp/mailchimp_marketing");
+
+
+
+const app = express();
+
+
+client.setConfig({
+    apiKey: "59a1485b676b0e3f48f2740a4ffa259b-us1",
+    server: "us1",
+});
+
+/* Anstelle von Body Parser wird der interne Parser genutzt */
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+/* Directory als öffentlich kennzeichnen für CSS und Bilder */
+app.use(express.static("public"));
+
+
+/* Standard Listener für den Server und Port */
+app.listen(3000, function () {
+
+    console.log("Ready");
+})
+
+
+/* Haupt GET Aufruf für die Homeseite */
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+
+/* Übermittlung von Formularen via POST. 
+Zusätzlich muss auf der Form eine METHOD und eine ACTION gekennzeichnet sein */
+app.post("/", function (req, res) {
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+
+    const run = async () => {
+        const response = await client.lists.addListMember("cccd28e784", {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }, update_existing: true
+        });
+
+        console.log("gesendet");
+
+        console.log(response.id);
+    };
+    res.sendFile(__dirname + "/success.html")
+
+
+    run().catch(e => res.sendFile(__dirname + "/failure.html"));
+})
+
+/*
+59a1485b676b0e3f48f2740a4ffa259b-us1 */
+
+/* audience id: cccd28e784 */
